94679df503c2eec03e92dcda869bddd327372ca7
Cicera Ribeiro <maccicera@gmail.com>
2025-02-03 13:02:50 -0300

    feat: ajuste de navegação

--- generator/src/components/MenuItems/MenuItems.tsx
+++ generator/src/components/MenuItems/MenuItems.tsx
@@ -1,15 +1,17 @@
-import Link, { LinkProps } from "next/link";
+import Link from "next/link";
 import { ReactNode } from "react";
 
-export interface MenuProps extends LinkProps {
+export interface MenuProps {
     title: string;
     icon: ReactNode;
+    href: string;
+    onClick?:() => void
 }
 
-export default function MenuItems({ title, icon, ...rest }: MenuProps) {
+export default function MenuItems({ title, icon, href, onClick }: MenuProps) {
     return (
         <div className="mb-4">
-            <Link {...rest} className="flex items-center space-x-3 p-2 rounded-md hover:bg-gray-700 transition">
+            <Link href={href} className="flex items-center space-x-3 p-2 rounded-md hover:bg-gray-700 transition">
                 <span className="text-xl">{icon}</span>
                 <span className="text-white">{title}</span>
             </Link>
